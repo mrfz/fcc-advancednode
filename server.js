41b8025dd3dd0f6f3cea4917770509e4612b3f3c
Fedor Zlobin <zlobin.f@gmail.com>
2022-05-17 08:57:36 +0000

    fccTask: logoul and 40 handling

--- server.js
+++ server.js
@@ -55,6 +55,19 @@ myDB(async client => {
           }
 );
 
+  app.get('/logout',
+      (req, res) => {
+        req.logout();
+        res.redirect('/');
+      }
+  );
+
+  app.use((req, res, next) => {
+  res.status(404)
+    .type('text')
+    .send('Not Found');
+});
+  
   app.use(session({
     secret: process.env.SESSION_SECRET,
     resave: true,
