7956b2d5ec16d4a4ad02d5788ad6969d3358bd88
Fedor Zlobin <zlobin.f@gmail.com>
2022-05-18 06:07:33 +0000

    fccTask: gitHub Auth part 2

--- auth.js
+++ auth.js
@@ -1,7 +1,9 @@
 let session = require('express-session');
 let passport = require('passport');
 const LocalStrategy = require('passport-local');
+const GitHubStrategy = require('passport-github').Strategy;
 const bcrypt = require('bcrypt');
+require('dotenv').config();
 
 module.exports = (app, myDataBase) => {
 
@@ -25,19 +27,27 @@ module.exports = (app, myDataBase) => {
     });
   
   });
-
+    //Using Local Strategy
     passport.use(new LocalStrategy(
-  function(username, password, done) {
-    
-    myDataBase.findOne({ username: username }, function (err, user) {
-      console.log('User '+ username +' attempted to log in.');
-      if (err) { return done(err); }
-      if (!user) { return done(null, false); }
-      if (!bcrypt.compareSync(password, user.password)) { 
-        return done(null, false);
+      function(username, password, done) {
+        myDataBase.findOne({ username: username }, function (err, user) {
+          console.log('User '+ username +' attempted to log in.');
+          if (err) { return done(err); }
+          if (!user) { return done(null, false); }
+          if (!bcrypt.compareSync(password, user.password)) { 
+            return done(null, false);
+          }
+          return done(null, user);
+        });
       }
-      return done(null, user);
-    });
-  }
-  ));
+    ));
+    //Using github strategy
+    passport.use(new GitHubStrategy({
+      clientID: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      callbackURL: "https://fcc-advancednode.mrfz.repl.co/auth/github/callback"
+      },
+      function GitHubAuthProcess(accessToken, refreshToken, profile, cb) {
+        console.log(profile);
+      }))
 }
