aee8d5ae1d35bcd895d83e4510fdc311036944f0
Fedor Zlobin <zlobin.f@gmail.com>
2022-05-21 19:59:48 +0000

    fccTask: authorize with socketIO completed

--- server.js
+++ server.js
@@ -19,6 +19,12 @@ const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
+const passportSocketIo = require('passport.socketio');
+const MongoStore = require('connect-mongo')(session);
+const cookieParser= require('cookie-parser');
+const URI = process.env.MONGO_URI;
+const store = new MongoStore({ url: URI});
+
 fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
@@ -28,13 +34,37 @@ app.set('view engine', 'pug')
 app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: true,
+  key: 'express.sid',
+  store: store,
   saveUninitialized: true,
   cookie: { secure: false }
 }));
 app.use(passport.initialize());
 app.use(passport.session());  
 
+function onAuthorizeSuccess(data, accept) {
+  console.log('successful connection to socket.io');
 
+  accept(null, true);
+}
+
+function onAuthorizeFail(data, message, error, accept) {
+  if (error) throw new Error(message);
+  console.log('failed connection to socket.io:', message);
+  accept(null, false);
+}
+
+//creating session with socketIO
+io.use(
+  passportSocketIo.authorize({
+    cookieParser: cookieParser,
+    key: 'express.sid',
+    secret: process.env.SESSION_SECRET,
+    store: store,
+    success: onAuthorizeSuccess,
+    fail: onAuthorizeFail
+  })
+);
 
 myDB(async client => {
   const myDataBase = await client.db('database').collection('users');
@@ -44,15 +74,16 @@ myDB(async client => {
 
 
 
+
   //countng users
   let currentUsers = 0;
   io.on('connection', socket => {
-    console.log('A user has connected');
+    console.log('user ' + socket.request.user.name + ' connected');
     ++currentUsers;
     io.emit('user count', currentUsers);
 
-    socket.on('disconnect', () => {--currenUsers; console.log('User disconneted')})
-    io.emit('disconnect', currentUsers);
+    socket.on('disconnect', () => {--currentUsers; console.log('User disconneted')})
+    
     
   });
 
